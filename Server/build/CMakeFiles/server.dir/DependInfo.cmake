d2364bdeb22b8a0d6fe775faadf648b3e2d6eade
mateocapon <mcapon@fi.uba.ar>
2021-06-12 17:08:34 -0300

    agrego box2d para que todos lo tengamos y ejemplo de juguete en server probando

---
+++ Server/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/matecapo67/Documents/Facultad/2021 1er cuatri/Taller de Programacion/taller-tpFinal-1c2021/Server/src/main.cpp" "/home/matecapo67/Documents/Facultad/2021 1er cuatri/Taller de Programacion/taller-tpFinal-1c2021/Server/build/CMakeFiles/server.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././libs/box2d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Server/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+
+Por ahora solo vemos ejemplos de box 2d para ir aprendiendo.
+
+*/
+
+#include "../../libs/box2d/include/box2d/b2_world.h"
+#include <iostream>
+
+
+int main() {
+
+	std::cout << "Hola" <<std::endl;
+
+	return 0;
+}

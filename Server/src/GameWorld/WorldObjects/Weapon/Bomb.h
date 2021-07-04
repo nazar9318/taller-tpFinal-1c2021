60fa9af1c41126627222fa9b1870c47840da920e
mateocapon <65830097+mateocapon@users.noreply.github.com>
2021-07-04 02:09:08 -0300

    Update Bomb.h
    Escribo desde el celu por eso esta todo comentado.  Despues lo pruebo todo buenas noches

--- Server/src/GameWorld/WorldObjects/Weapon/Bomb.h
+++ Server/src/GameWorld/WorldObjects/Weapon/Bomb.h
@@ -1,29 +1,94 @@
 #ifndef BOMB_H
 #define BOMB_H
 /*
-#include "Weapon.h"
-#include "Configuration.h"
-#include <iostream>
+#include "TypesOfEvents.h"
 
-class Character;
 
-class Bomb : public Weapon {
+enum BombState: char {
+    NORMAL,
+    ACTIVATED, 
+    DEACTIVATED, 
+    ACTIVAITING, 
+    DEACTIVATING, 
+    EXPLOTED
+};
+
+class Bomb {
     private:
-        uint16_t clock_count;
-        bool activated;
-        bool placed;
-        void shoot(Character &character, uint16_t distance);
+        int clock_count;
+        BombState state;
+        bool has_owner;
+        char id_owner;
+        int x_pos;
+        int y_pos;
 
     public:
         Bomb();
+                
+        void simulate_step() {
+            switch (state) {
+                case BombState::ACTIVATED:
+                {
+                    clock_count++;
+                    if (clock_count * TIME_STEP >= TIME_BOMB_EXPLOTE)
+                        state = BombState::EXPLOTED;
+                }                
+                case BombState::ACTIVAITING:
+                {
+                    clock_count++;
+                    if (clock_count * TIME_STEP >= TIME_BOMB_ACTIVATE) {
+                       state = BombState::ACTIVATED;
+                       clock_count = 0;
+                    }    
+                }
+                case BombState::DEACTIVATING: 
+                {
+                    clock_count++;
+                    if (clock_count * TIME_STEP >= TIME_BOMB_DEACTIVATE)
+                        state = BombState::DEACTIVATED;
+                }              
+            }
+            
+        }
         
-        void tickTime();
+        bool activate(char id) {
+            if (has_owner && (id_owner == id)) {
+                state = BombState::ACTIVATING;
+                clock_count = 0;
+                return true;
+            }
+            return false;
+        }
+        
+        bool drop(char id, int x, int y) {
+            if (id == id_owner) {
+                has_owner = false;
+                state = BombState::NORMAL;
+                pos_x = x;
+                pos_y = y;
+                clock_count = 0;
+                return true;
+            }
+            return false;
+        }
+        
+        bool grab(char id, Team team) {
+            if ((!has_owner) && (team == Team::TERRORIST)) {
+                id_owner = id;
+                clock_count = 0;
+                has_owner = true;
+                return true;
+            }
+            return false;
+        }
 
-        bool countdownEnded();
-
-        void deactivate();
-
-        void place();
+        bool deactivate(Team team) {
+            if (team == Team::COUNTER_ENEMY) {
+                state = BombState::DEACTIVATING;
+                return true;
+            }
+            return false;
+        }
 
         ~Bomb();
 };
